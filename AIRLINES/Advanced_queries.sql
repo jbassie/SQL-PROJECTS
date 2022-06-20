33b9bec797186c60eb03d1db5e6c23b4adc34193
John Bassey <jsbazman@gmail.com>
2022-06-20 07:42:56 +0100

    Update Advanced_queries.sql

--- AIRLINES/Advanced_queries.sql
+++ AIRLINES/Advanced_queries.sql
@@ -39,3 +39,14 @@ FROM (SELECT s.seat_no FROM seats s
 												   WHERE flight_no = 'PG0404'
 							 		 AND scheduled_departure::date = public.now()::date - INTERVAL '1 day')) as t
 	
+/* 4.Find the most disciplined passengers who checked in first for all their flights. Take into account only those
+passengers who took at least two flights */
+	SELECT 
+		t. passenger_name, t.ticket_no
+	FROM
+		tickets t
+	JOIN
+		boarding_passes bp
+	USING (ticket_no)
+	GROUP BY 1,2
+	HAVING MAX(bp.boarding_no) = 2 and Count(*) >1
